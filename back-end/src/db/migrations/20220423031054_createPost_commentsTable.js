eb3e4a38393659f9998d1d320eea8cbfd1b3b7d3
admclamb <adylanmclamb@yahoo.com>
2022-04-22 23:15:56 -0400

    change database table post_comments

---
+++ back-end/src/db/migrations/20220423031054_createPost_commentsTable.js
@@ -0,0 +1,15 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+  return knex.schema.createTable("post_comments", () => {
+    table.string();
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {};
--- back-end/src/utils/hasOnlyValidProperties.js
+++ back-end/src/utils/hasOnlyValidProperties.js
@@ -1,4 +1,5 @@
 function hasOnlyValidProperties(properties) {
+  console.log(properties);
   return function (req, res, next) {
     const { data = {} } = req.body;
     const invalidFields = Object.keys(data).filter(
